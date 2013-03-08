e0f580c8bdec92a6581cb1ce278b399a35b1b7e2
mirelon <miso@github.ksp.sk>
2013-03-08 10:36:34 +0100

    New app with devise set up on users

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lunchkeeper::Application.config.secret_token = '77f47f2d2107209fe29dd5710e58c4d0c8d9946d07a3553937cf5ba4acb5531b22dfce8b0c31f5b493014732f9e89ac286bba5f11d15bd7ba2bf61e949f3bc8e'
